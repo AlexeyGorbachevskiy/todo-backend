4be48b3c83e7edf34484104c37390ba96f605605
AlexeyGorbachevskiy <alexeygorbachevskiyy@gmail.com>
2021-03-26 21:45:22 +0300

    fix: add headers to node js

--- app.js
+++ app.js
@@ -11,6 +11,20 @@ app.use('/api/todos', require('./routes/todos.routes'));
 app.use('/api/tasks', require('./routes/tasks.routes'));
 app.use('/api/drag&Drop', require('./routes/dragDrop.routes'));
 app.use(cors());
+// Add headers
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    // Pass to next layer of middleware
+    next();
+});
 
 // const PORT = config.get('port') || 5000;
 const PORT = process.env.PORT || 5000;
